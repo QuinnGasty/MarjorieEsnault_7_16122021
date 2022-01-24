e9ad702ccaf46eb1e1a0a4ca5b5d9db71ee851b9
Marjorie Esnault <gastyquinn@gmail.com>
2022-01-24 19:07:22 +0100

    Unit display
    Affichage des appareils dans le dropdown

---
+++ Javascript/display-units.js
@@ -0,0 +1,22 @@
+export const listofUnits = (mealsList) => {
+    let unitList = [];
+
+    mealsList.map((meal) => {
+        unitList.push(meal.appliance);
+
+        unitList = [...new Set(unitList)]
+    })
+
+    return unitList
+}
+
+export const unitDisplay = (unitList) => {
+    const tagsUnit = document.querySelector(".tags-drop-units");
+    let tagUnitHTML = "";
+
+    unitList.forEach(unit => {
+        tagUnitHTML += `<li><span class="tag-elt" data-bg='unit'>${unit}</span></li>`
+    });
+
+    tagsUnit.innerHTML = tagUnitHTML;
+}
